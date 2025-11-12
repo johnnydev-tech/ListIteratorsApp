45b41455376af36b497d4d44a86523eb365fb19a
johnnydev-tech <johnnyfreirejh@hotmail.com>
2025-11-12 19:37:32 -0300

    Initial Commit

---
+++ ListIteratorsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ListIteratorsApp
+//
+//  Created by Johnny Freire on 12/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ListIteratorsApp/ListIteratorsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListIteratorsAppApp.swift
+//  ListIteratorsApp
+//
+//  Created by Johnny Freire on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ListIteratorsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
