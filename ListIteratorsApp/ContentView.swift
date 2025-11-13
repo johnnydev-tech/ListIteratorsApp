415578bd663638ccbcf77a02310455f42d9f2fc1
johnnydev-tech <johnnyfreirejh@hotmail.com>
2025-11-12 21:03:06 -0300

    feat: create ui with navigation

--- ListIteratorsApp/ContentView.swift
+++ ListIteratorsApp/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    let persons: [PersonEntity] = [
+        PersonEntity(name: "Johnny Freire", favoriteColor: Color.black),
+        PersonEntity(name: "Yoda" , favoriteColor: Color.green),
+        PersonEntity(name: "Obi-Wan Kenobi", favoriteColor: Color.blue),
+        PersonEntity(name: "Darth Vader", favoriteColor: Color.red),
+        
+        
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        let sortedPersons: [PersonEntity] = persons.sorted(by: { $0.name < $1.name })
+        NavigationStack{
+            List {
+                ForEach(sortedPersons, id: \.id) { person in
+                    NavigationLink{
+                        DetailsItemView(personEntity: person)
+                    }
+                    label: { ListCardComponent(personEntity: person)
+                    }
+                }
+            }.navigationTitle(Text("Perfis"))
         }
-        .padding()
     }
 }
 
---
+++ ListIteratorsApp/DetailsItemView.swift
@@ -0,0 +1,38 @@
+//
+//  DetailsItemView.swift
+//  ListIteratorsApp
+//
+//  Created by Johnny Freire on 12/11/25.
+//
+
+import SwiftUI
+
+struct DetailsItemView: View {
+    let personEntity: PersonEntity
+    var body: some View {
+        VStack {
+            Text("Olá! Meu nome é \(personEntity.name)")
+                .font(Font.largeTitle)
+            
+            Spacer()
+            
+            HStack(alignment: .center) {
+                Text("E minha cor favorita é")
+                Text(personEntity.colorName)
+                    .font(.title2)
+                    .bold()
+                    .foregroundStyle(personEntity.favoriteColor)
+            }
+            .padding(.horizontal)
+            
+            Spacer()
+        }
+        .frame(maxHeight: .infinity,
+               alignment: .top)
+        .padding()
+    }
+}
+
+#Preview {
+    DetailsItemView(personEntity: PersonEntity(name: "Johnny Freire", favoriteColor: Color.black))
+}
